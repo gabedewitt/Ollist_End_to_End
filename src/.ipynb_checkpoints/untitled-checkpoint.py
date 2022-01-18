d138a32cac64bf135b06e321e8e7879fdfe74f15
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2022-01-18 07:13:55 -0300

    Inicio da segmetação de Clientes

---
+++ src/.ipynb_checkpoints/untitled-checkpoint.py
@@ -0,0 +1,74 @@
+#defining visualizaition functions
+def format_spines(ax, right_border=True):
+    
+    ax.spines['bottom'].set_color('#666666')
+    ax.spines['left'].set_color('#666666')
+    ax.spines['top'].set_visible(False)
+    if right_border:
+        ax.spines['right'].setS_color('#FFFFFF')
+    else:
+        ax.spines['right'].set_color('#FFFFFF')
+    ax.patch.set_facecolor('#FFFFFF')
+    
+
+def count_plot(feature, df, colors='Blues_d', hue=False, ax=None, title=''):
+    
+    # Preparing variables
+    ncount = len(df)
+    if hue != False:
+        ax = sns.countplot(x=feature, data=df, palette=colors, hue=hue, ax=ax)
+    else:
+        ax = sns.countplot(x=feature, data=df, palette=colors, ax=ax)
+        
+    format_spines(ax)
+
+    # Setting percentage
+    for p in ax.patches:
+        x=p.get_bbox().get_points()[:,0]
+        y=p.get_bbox().get_points()[1,1]
+        ax.annotate('{:.1f}%'.format(100.*y/ncount), (x.mean(), y), 
+                ha='center', va='bottom') # set the alignment of the text
+    
+    # Final configuration
+    if not hue:
+        ax.set_title(df[feature].describe().name + ' Analysis', size=13, pad=15)
+    else:
+        ax.set_title(df[feature].describe().name + ' Analysis by ' + hue, size=13, pad=15)  
+    if title != '':
+        ax.set_title(title)       
+    plt.tight_layout()
+    
+    
+def bar_plot(x, y, df, colors='Blues_d', hue=False, ax=None, value=False, title=''):
+    
+    # Preparing variables
+    try:
+        ncount = sum(df[y])
+    except:
+        ncount = sum(df[x])
+    #fig, ax = plt.subplots()
+    if hue != False:
+        ax = sns.barplot(x=x, y=y, data=df, palette=colors, hue=hue, ax=ax, ci=None)
+    else:
+        ax = sns.barplot(x=x, y=y, data=df, palette=colors, ax=ax, ci=None)
+
+    # Setting borders
+    format_spines(ax)
+
+    # Setting percentage
+    for p in ax.patches:
+        xp=p.get_bbox().get_points()[:,0]
+        yp=p.get_bbox().get_points()[1,1]
+        if value:
+            ax.annotate('{:.2f}k'.format(yp/1000), (xp.mean(), yp), 
+                    ha='center', va='bottom') # set the alignment of the text
+        else:
+            ax.annotate('{:.1f}%'.format(100.*yp/ncount), (xp.mean(), yp), 
+                    ha='center', va='bottom') # set the alignment of the text
+    if not hue:
+        ax.set_title(df[x].describe().name + ' Analysis', size=12, pad=15)
+    else:
+        ax.set_title(df[x].describe().name + ' Analysis by ' + hue, size=12, pad=15)
+    if title != '':
+        ax.set_title(title)  
+    plt.tight_layout()
